d5f9f3d9507035ae6a7f9799ebb1dc13e1c073d6
jazmin <jazminisabeldestefano@gmail.com>
2023-08-28 20:25:11 -0300

    add process.env

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 
 const app = express();
-
-const MONGO_DB =
-  "mongodb+srv://admin:IG0axpmnSGu3Npg3@cluster0.a9hnh6c.mongodb.net/BookStore?retryWrites=true&w=majority";
 
 // Middlewares
 app.use(express.json());
@@ -21,7 +19,7 @@ app.use(
 app.use("/books", router); // localhost:5000/books
 
 mongoose
-  .connect(MONGO_DB)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("connected to db"))
   .then(() => {
     app.listen(5000);
